111b40cf5ac61e2b9773ee6337f69dd4b11be945
kainat-anjum <61592772+kainat-anjum@users.noreply.github.com>
2023-02-04 13:58:08 +0500

    creates common variables

---
+++ src/commons/variables.js
@@ -0,0 +1,29 @@
+export const size = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+export const device = {
+  mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
+  mobileL: `(max-width: ${size.mobileL})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
+  desktop: `(max-width: ${size.desktop})`,
+  desktopL: `(max-width: ${size.desktop})`,
+};
+
+export const colors = {
+  primaryColor: "#ff6c59",
+  secondaryColor: "#2ce6c8",
+  whiteColor: "#fff",
+  grayColor: "#ccc",
+  alphaGreyColor:"#cccccc75",
+  blackColor: "#2c3046",
+};
